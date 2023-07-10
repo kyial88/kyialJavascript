46e41c7d6598d308ba4d9a5ca5eecb85ea11e85a
kyial88 <klibraeva@gmail.com>
2023-07-10 13:33:19 +0600

    byi

---
+++ Script.js
@@ -0,0 +1,52 @@
+//  let a = 'hello'
+//  let b = 'Kyial,42'
+// console.log(a+b)
+
+// let a = 4
+//  let b = 55
+//   console.log(a+b)
+//  let a=4
+//  let b=55
+
+ // console.log(a+b)
+
+// let a = 4
+// let b = 55
+// console.log(a*b)
+
+// let a = 4
+// let b = 55
+// console.log(a/b)
+
+// let a = 4
+// let b = 55
+// console.log(4%55)
+
+// let a = 'hello'
+// let b = 'kyial'
+// console.log(a+b)
+
+// let a = 4
+// let b = 'dan'
+
+// console.log(a*b)
+
+// let a = 5
+// let b = '5'
+// console.log(+b+a)
+
+let c = 2
+let b = undefined
+console.log(b * c % 2 ===0)
+
+
+
+
+
+
+
+
+
+
+
+
